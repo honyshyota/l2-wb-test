50077b0551ace55f0cb48b817590cda6e742f040
honyshyota <semensuprunenko@gmail.com>
2022-08-18 16:07:08 +0300

    ♻️ Refactor test

--- develop/dev02/extract_test.go
+++ develop/dev02/extract_test.go
@@ -11,41 +11,53 @@ func TestExtract_Extract(t *testing.T) {
 	testCases := []struct {
 		name    string
 		val     string
+		equal   string
 		isValid bool
 	}{
 		{
 			name:    "with digits",
 			val:     "a4bc2d5e",
+			equal:   "aaaabccddddde",
 			isValid: true,
 		},
 		{
 			name:    "letters only",
 			val:     "abcd",
+			equal:   "abcd",
+			isValid: true,
+		},
+		{
+			name:    "digits only",
 			isValid: true,
 		},
 		{
 			name:    "digits only",
 			val:     "45",
+			equal:   "",
 			isValid: false,
 		},
 		{
 			name:    "empty",
 			val:     "",
+			equal:   "",
 			isValid: true,
 		},
 		{
 			name:    "with shield 1",
 			val:     `qwe\4\5`,
+			equal:   "qwe45",
 			isValid: true,
 		},
 		{
 			name:    "with sield 2",
 			val:     `qwe\45`,
+			equal:   "qwe44444",
 			isValid: true,
 		},
 		{
 			name:    "with shield 3",
 			val:     `qwe\\5`,
+			equal:   `qwe\\\\\`,
 			isValid: true,
 		},
 	}
@@ -53,10 +65,12 @@ func TestExtract_Extract(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			if tc.isValid {
-				_, err := extract.Extract(tc.val)
+				val, err := extract.Extract(tc.val)
+				assert.Equal(t, val, tc.equal)
 				assert.NoError(t, err)
 			} else {
-				_, err := extract.Extract(tc.val)
+				val, err := extract.Extract(tc.val)
+				assert.Equal(t, tc.equal, val)
 				assert.Error(t, err)
 			}
 		})
